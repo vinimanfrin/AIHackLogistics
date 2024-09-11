0e4a5a344bc9e2c3492be36740645e240c735a4d
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-09-11 20:25:29 -0300

    FEAT: Cria e estiliza a Pagina inicial

---
+++ src/main/java/com/logistcs/aihack/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.logistcs.aihack.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String home(){
+        return "index";
+    }
+}
