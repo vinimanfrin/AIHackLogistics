3fb16b752b3dd5b89826df74206422609ce1fc1b
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-09-14 19:06:12 -0300

    FIX: ajustes nas validações de cep

--- src/main/resources/static/js/endereco.js
+++ src/main/resources/static/js/endereco.js
@@ -1,28 +1,29 @@
 let cepValido = false;
 
-function buscarCep() {
+async function buscarCep() {
     const cep = document.getElementById('cep').value.replace(/\D/g, '');
 
     if (cep.length === 8) {
-        fetch(`https://viacep.com.br/ws/${cep}/json/`)
-            .then(response => response.json())
-            .then(data => {
-                if (!data.erro) {
-                    document.getElementById('rua').value = data.logradouro;
-                    document.getElementById('bairro').value = data.bairro;
-                    document.getElementById('cidade').value = data.localidade;
-                    document.getElementById('estado').value = data.uf;
-                    cepValido = true;
-                } else {
-                    alert("CEP não encontrado!");
-                    limparCamposEndereco();
-                    cepValido = false;
-                }
-            })
-            .catch(error => {
-                console.error("Erro ao buscar o CEP:", error);
+        try {
+            const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+            const data = await response.json();
+
+            if (!data.erro) {
+                document.getElementById('rua').value = data.logradouro;
+                document.getElementById('bairro').value = data.bairro;
+                document.getElementById('cidade').value = data.localidade;
+                document.getElementById('estado').value = data.uf;
+                cepValido = true;
+            } else {
+                alert("CEP não encontrado!");
                 limparCamposEndereco();
-            });
+                cepValido = false;
+            }
+        } catch (error) {
+            console.error("Erro ao buscar o CEP:", error);
+            limparCamposEndereco();
+            cepValido = false;
+        }
     } else {
         alert("CEP inválido!");
         limparCamposEndereco();
@@ -37,10 +38,18 @@ function limparCamposEndereco() {
     document.getElementById('estado').value = '';
 }
 
-function validarFormulario(event) {
-    if (!cepValido) {
+async function validarFormulario(event) {
+    const cep = document.getElementById('cep').value.replace(/\D/g, '');
+
+    if (cep.length === 8 && !cepValido) {
+        await buscarCep();  // Espera a validação do CEP ser concluída
+        if (!cepValido) {
+            alert("Por favor, insira um CEP válido antes de continuar.");
+            event.preventDefault();  // Impede o envio do formulário se o CEP não for válido
+        }
+    } else if (!cepValido) {
         alert("Por favor, insira um CEP válido antes de continuar.");
-        event.preventDefault(); // Impede o envio do formulário se o CEP não for válido
+        event.preventDefault();  // Impede o envio do formulário se o CEP não for válido
     }
 }
 
@@ -48,3 +57,5 @@ function inicializarCepHandler() {
     document.getElementById('cep').addEventListener('blur', buscarCep);
     document.querySelector('form').addEventListener('submit', validarFormulario);
 }
+
+window.addEventListener('load', inicializarCepHandler)
